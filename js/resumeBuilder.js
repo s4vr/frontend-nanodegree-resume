0892f85d738e4adc9386643664c8396f83733f42
abraham <s4vr@outlook.com>
2015-11-09 20:04:01 -0600

    mensaje de prueba

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,7 +6,7 @@ $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 var bio = {
 	"contacts": {
-		"mobile":"442154854531",
+		"mobile":"44215485453",
 		"email": "s4vr@outlook.com",
 		"github": "s4vr",
 		"twitter": "@_abrahamsc",
